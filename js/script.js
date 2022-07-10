eff3f319672ad31ed7c85f17725ad1f1be68d810
Grzegorz Radziszewski <greg.rad@yahoo.com>
2022-07-10 13:27:55 +0200

    Add changes in clicked Element

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ for(let activeLink of activeLinks){
   /* add class 'active' to the clicked link */
   for(let activeLink of activeLinks){
     activeLink.classList.add('active');
-    console.log('clickedElement (with plus): ' + clickedElement);
+  console.log('clickedElement:', clickedElement);
 
   /* remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('a.active');

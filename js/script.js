569db2e4ed846acd903661e2bd0c5eb6e57d0dc3
Grzegorz Radziszewski <greg.rad@yahoo.com>
2022-08-02 19:39:01 +0200

    Add new excercise

--- js/script.js
+++ js/script.js
@@ -35,6 +35,7 @@ const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles';
 
+// eslint-disable-next-line no-unused-vars
 function generateTitleLinks() {
 
   const titleList = document.querySelector(optTitleListSelector);
@@ -61,7 +62,8 @@ function generateTitleLinks() {
     const articleTitle = article.querySelector(optTitleSelector).innerHTML;
     const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle +'</span></a></li>';
     titleList.innerHTML = titleList.innerHTML + linkHTML;
-  
+    // eslint-disable-next-line no-unused-vars
+    let html = '';
     const links = document.querySelectorAll('.titles a');
     for (const link of links) {
       link.addEventListener('click', titleClickHandler);
@@ -70,3 +72,33 @@ function generateTitleLinks() {
 
   generateTitleLinks();
 }
+function generateTags(){
+  /* find all articles */
+  const optArticleTagsSelector = '.post-tags .list',
+  console.log('split');
+  
+
+  /* START LOOP: for every article: */
+
+  /* find tags wrapper */
+
+  /* make html variable with empty string */
+
+  /* get tags from data-tags attribute */
+
+  /* split tags into array */
+
+  /* START LOOP: for each tag */
+
+  /* generate HTML of the link */
+
+  /* add generated code to html variable */
+
+  /* END LOOP: for each tag */
+
+  /* insert HTML of all the links into the tags wrapper */
+
+  /* END LOOP: for every article: */
+}
+generateTags();
+
